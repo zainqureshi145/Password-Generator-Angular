d068c9da1f68d35f75c28a2a69932d7817f7d0ab
Zain Qureshi <zainqureshi145@gmail.com>
2021-03-10 17:52:50 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pass-gen';
+  password = '';
+
+  onButtonClick() {
+    this.password = "Your Password";
+  }
 }
