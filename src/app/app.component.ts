cee33476748f09e0e1cc2fef86d3a9a2f8ddc897
Zain Qureshi <zainqureshi145@gmail.com>
2021-03-11 15:28:22 +0500

    How Event Handlers Work in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
 
-  onButtonClick() {
+  onButtonClickHandler() {
+    console.log(this.useLetters);
+    console.log(this.useNumbers);
+    console.log(this.useSymbols);
     this.password = "Your Password";
+  }
+  onChangeLettersHandler() {
+    this.useLetters = !this.useLetters;
+  }
+  onChangeNumbersHandler() {
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeSymbolsHandler() {
+    this.useSymbols = !this.useSymbols;
   }
 
   genPassword() {
     return this.password;
+  }
+
+  getName() {
+    return 'Alexa';
   }
 }
